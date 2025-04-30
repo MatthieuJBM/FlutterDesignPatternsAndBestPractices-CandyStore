7e8bdb9a0f787cd2bcc33fa0284fff66e4403bd5
Mateusz Baryla <m.baryla@protonmail.ch>
2025-05-01 00:20:52 +0200

    feat: Cart notifier implemented.

---
+++ lib/cart_notifier.dart
@@ -0,0 +1,50 @@
+import 'package:candy_store/cart_list_item.dart';
+import 'package:candy_store/product_list_item.dart';
+import 'package:flutter/cupertino.dart';
+
+class CartNotifier extends ChangeNotifier {
+  final Map<String, CartListItem> _items = {};
+  double _totalPrice = 0;
+  int _totalItems = 0;
+
+  List<CartListItem> get items => _items.values.toList();
+
+  double get totalPrice => _totalPrice;
+
+  int get totalItems => _totalItems;
+
+  void addToCart(ProductListItem item) {
+    _items.update(
+      item.id,
+      (existingItem) => CartListItem(
+        product: existingItem.product,
+        quantity: existingItem.quantity + 1,
+      ),
+      ifAbsent: () => CartListItem(
+        product: item,
+        quantity: 1,
+      ),
+    );
+
+    notifyListeners();
+  }
+
+  void removeFromCart(CartListItem item) {
+    final CartListItem? existingItem = _items[item.product.id];
+    if (existingItem == null) return;
+
+    if (existingItem.quantity > 1) {
+      _items.update(
+        item.product.id,
+        (existingItem) => CartListItem(
+          product: existingItem.product,
+          quantity: existingItem.quantity - 1,
+        ),
+      );
+    } else {
+      _items.remove(item.product.id);
+    }
+
+    notifyListeners();
+  }
+}
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,7 +1,7 @@
 import 'package:candy_store/cart_button.dart';
 import 'package:candy_store/cart_list_item.dart';
+import 'package:candy_store/cart_notifier.dart';
 import 'package:candy_store/cart_page.dart';
-import 'package:candy_store/product_list_item.dart';
 import 'package:candy_store/products_page.dart';
 import 'package:flutter/material.dart';
 
@@ -13,23 +13,34 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  CartNotifier cartNotifier = CartNotifier();
   final List<CartListItem> cartItems = [];
 
   // The Map key is the id of the CartListItem. We will use a Map data structure
   // because it is easier to manage the addition, removal & count of the items.
-  final Map<String, CartListItem> cartItemsMap = {};
+  // final Map<String, CartListItem> cartItemsMap = {};
+  ValueNotifier<Map<String, CartListItem>> items = ValueNotifier({});
+
+  @override
+  void initState() {
+    super.initState();
+    cartNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    cartNotifier.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final totalCount = cartItemsMap.values.fold<int>(
-      0,
-      (previousValue, element) => previousValue + element.quantity,
-    );
-
     return Stack(
       children: [
         ProductsPage(
-          onAddToCart: addToCart,
+          onAddToCart: cartNotifier.addToCart,
         ),
         Positioned(
           right: 16,
@@ -37,7 +48,7 @@ class _MainPageState extends State<MainPage> {
           child: GestureDetector(
             onTap: openCart,
             child: CartButton(
-              count: totalCount,
+              count: cartNotifier.totalItems,
             ),
           ),
         ),
@@ -45,50 +56,12 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  void addToCart(ProductListItem item) {
-    setState(() {
-      cartItemsMap.update(
-        item.id,
-        (existingItem) => CartListItem(
-          product: existingItem.product,
-          quantity: existingItem.quantity + 1,
-        ),
-        ifAbsent: () => CartListItem(
-          product: item,
-          quantity: 1,
-        ),
-      );
-    });
-  }
-
-  void removeFromCart(CartListItem item) {
-    setState(() {
-      final CartListItem? existingItem = cartItemsMap[item.product.id];
-      if (existingItem == null) return;
-
-      if (existingItem.quantity > 1) {
-        cartItemsMap.update(
-          item.product.id,
-          (existingItem) => CartListItem(
-            product: existingItem.product,
-            quantity: existingItem.quantity - 1,
-          ),
-        );
-      } else {
-        cartItemsMap.remove(item.product.id);
-      }
-    });
-  }
-
   void openCart() {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) => CartPage(
-          items: cartItemsMap.values.toList(),
-          onAddToCart: (item) {
-            addToCart(item.product);
-          },
-          onRemoveFromCart: removeFromCart,
+          cartNotifier: cartNotifier,
+          items: items,
         ),
       ),
     );
